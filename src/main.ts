adac1f98f20c3c233e9f50a8116ea38af1487abb
Luca Armaroli <lukaarma@gmail.com>
2020-10-22 13:28:03 +0200

    added Material Design Icons pack

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import '@mdi/font/css/materialdesignicons.min.css'
 
 import App from './App.vue';
 import router from './router';
