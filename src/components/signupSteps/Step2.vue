53a78b0e7f55ccae40e8f39a2e409f8967b2e172
Luca Armaroli <lukaarma@gmail.com>
2020-10-24 01:18:14 +0200

    completed user info UI logic
    Co-authored-by: Lorenzo Romio <lorenzo.romio@live.it>

--- src/components/signupSteps/Step2.vue
+++ src/components/signupSteps/Step2.vue
@@ -11,11 +11,8 @@
             <b-input
                 v-model="user.firstName"
                 :disabled="disableAll"
-                @input="
-                    clearError('firstName');
-                    firstNameValidation.done = false;
-                "
-                @blur="validateName('firstName')"
+                @input="clearError('firstName')"
+                @blur="validateFirstName"
                 type="text"
                 placeholder="first name"
             />
@@ -32,51 +29,189 @@
             <b-input
                 v-model="user.lastName"
                 :disabled="disableAll"
-                @input="
-                    clearError('lastName');
-                    lastNameValidation.done = false;
-                "
-                @blur="validateName('lastName')"
+                @input="clearError('lastName')"
+                @blur="validateLastName"
                 type="text"
                 placeholder="last name"
             />
         </b-field>
 
         <!-- BIRTH DATE -->
-        <b-field label="Select a date">
-            <b-datepicker placeholder="Click to select..."> </b-datepicker>
+        <b-field
+            label="Birthdate"
+            :type="{ 'is-danger': birthdateValidation.error }"
+        >
+            <template v-slot:message>
+                {{ birthdateValidation.errorMessage }}
+            </template>
+            <div class="has-text-centered">
+                <b-datepicker
+                    v-model="user.birthdate"
+                    :first-day-of-week="1"
+                    :max-date="birthdateValidation.maxDate"
+                    @input="clearError('birthdate')"
+                    @blur="validateBirthdate"
+                    append-to-body
+                    position="is-top-right"
+                />
+            </div>
         </b-field>
+
+        <!--PHONE-->
+        <b-field
+            label="Phone number"
+            :type="{ 'is-danger': phoneNumberValidation.error }"
+        >
+            <template v-slot:message>
+                {{ phoneNumberValidation.errorMessage }}
+            </template>
+            <VueTelInput
+                v-model="user.phoneNumber"
+                :inputClasses="{
+                    'input phoneInput': true,
+                    'is-danger': phoneNumberValidation.error,
+                }"
+                @validate="phoneChangeState"
+                @input="clearError('phoneNumber')"
+                @blur="validatePhoneNumber"
+                dynamicPlaceholder
+                validCharactersOnly
+            />
+        </b-field>
+
+        <!-- NEXT -->
+        <div class="buttons is-right">
+            <b-button
+                :type="{ 'is-loading': stepValidating }"
+                @click="goNextStep"
+                class="is-primary mt-4"
+            >
+                Next
+            </b-button>
+        </div>
     </b-step-item>
 </template>
 
+
 <script lang="ts">
 import { defineComponent, ref } from '@vue/composition-api';
+import { VueTelInput } from 'vue-tel-input';
 
 
 export default defineComponent({
-    setup() {
-
+    components: {
+        VueTelInput,
+    },
+    setup(props, { emit }) {
         const nameRegex = /^[\p{L}][ \p{L}'-]*[\p{L}]$/u
-
-        const firstNameValidation = ref({
-            error: false,
-            errorMessage: '',
-            done: false
-        });
-
-        const lastNameValidation = ref({
-            error: false,
-            errorMessage: '',
-            done: false
-        });
+        const notNameRegex = /[^\p{L} '-]/u
 
         const user = ref({
             firstName: '',
             lastName: '',
-            phoneNumber: ''
+            phoneNumber: '',
+            birthdate: undefined
         });
 
-        function clearError(field: 'firstName' | 'lastName'): void {
+        const stepValidating = ref(false);
+        const disableAll = ref(false);
+
+        const firstNameValidation = ref({
+            error: false,
+            errorMessage: ''
+        });
+        const lastNameValidation = ref({
+            error: false,
+            errorMessage: ''
+        });
+
+        const maxDate = new Date();
+        maxDate.setFullYear(maxDate.getFullYear() - 14)
+        const birthdateValidation = ref({
+            maxDate,
+            error: false,
+            errorMessage: ''
+        });
+
+        const phoneNumberValidation = ref({
+            error: false,
+            errorMessage: '',
+            isValid: false
+        });
+
+
+        function validateFirstName(): boolean {
+            if (user.value.firstName && nameRegex.test(user.value.firstName)) {
+                return true;
+            }
+            else if (user.value.firstName && notNameRegex.test(user.value.firstName)) {
+                firstNameValidation.value.errorMessage = 'Illegal character inserted. ' +
+                    'Plesae use only lowecase and uppercase letter, space, dash and apostrophe';
+            }
+            else if (user.value.firstName.length === 1) {
+                firstNameValidation.value.errorMessage = 'The name is too short';
+            }
+            else if (!user.value.firstName.length) {
+                firstNameValidation.value.errorMessage = 'Field is required!';
+            }
+
+            firstNameValidation.value.errorMessage ? firstNameValidation.value.error = true : null;
+
+            return false;
+        }
+
+        function validateLastName(): boolean {
+            if (user.value.lastName && nameRegex.test(user.value.lastName)) {
+                return true;
+            }
+            else if (user.value.lastName && notNameRegex.test(user.value.lastName)) {
+                lastNameValidation.value.errorMessage = 'Illegal character inserted. ' +
+                    'Plesae use only lowecase and uppercase letter, space, dash and apostrophe';
+            }
+            else if (user.value.firstName.length === 1) {
+                lastNameValidation.value.errorMessage = 'The name is too short';
+            }
+            else if (!user.value.lastName.length) {
+                lastNameValidation.value.errorMessage = 'Field is required!';
+            }
+
+            lastNameValidation.value.errorMessage ? lastNameValidation.value.error = true : null
+            return false;
+        }
+
+        function validateBirthdate(): boolean {
+            if (user.value.birthdate) {
+                return true;
+            }
+            else {
+                birthdateValidation.value.error = true;
+                birthdateValidation.value.errorMessage = 'Field is required!';
+            }
+
+            return false;
+        }
+
+        function phoneChangeState(args: { isValid: boolean }): void {
+            phoneNumberValidation.value.isValid = args.isValid;
+        }
+
+        function validatePhoneNumber(): boolean {
+            if (user.value.phoneNumber && phoneNumberValidation.value.isValid) {
+                return true;
+            }
+            else if (user.value.phoneNumber) {
+                phoneNumberValidation.value.errorMessage = 'This is not a valid phone number'
+            }
+            else {
+                phoneNumberValidation.value.errorMessage = 'This field is required'
+            }
+
+            phoneNumberValidation.value.error = true;
+
+            return false;
+        }
+
+        function clearError(field: 'firstName' | 'lastName' | 'birthdate' | 'phoneNumber'): void {
             if (field === 'firstName') {
                 firstNameValidation.value.error = false;
                 firstNameValidation.value.errorMessage = '';
@@ -85,47 +220,57 @@ export default defineComponent({
                 lastNameValidation.value.error = false;
                 lastNameValidation.value.errorMessage = '';
             }
-            // else if (field === 'repeatPassword') {
-            //     repeatPasswordError.value = false;
-            //     repeatPasswordErrorMessage.value = '';
-            // }
+            else if (field === 'birthdate') {
+                birthdateValidation.value.error = false;
+                birthdateValidation.value.errorMessage = '';
+            }
+            else if (field === 'phoneNumber') {
+                phoneNumberValidation.value.error = false;
+                phoneNumberValidation.value.errorMessage = '';
+            }
             else {
                 console.log(`Error clear not implemented for '${field}'!`);
             }
         }
 
-        function validateName(field: 'firstName' | 'lastName'): boolean {
+        async function goNextStep(): Promise<void> {
+            stepValidating.value = disableAll.value = true;
 
-            if (nameRegex.test(user.value[field])) {
-                clearError(field);
+            const firstNameOk = validateFirstName();
+            const lastNameOk = validateLastName();
+            const birthdateOk = validateBirthdate();
+            const phoneNumberOk = validatePhoneNumber();
 
-                return true;
-            }
-            else {
-                if (field === 'firstName') {
-                    firstNameValidation.value.error = true;
-                    firstNameValidation.value.errorMessage = 'Illegal character inserted. Plesae use only lowecase and uppercase letter, space, dash and apostrophe';
-                }
-                else if (field === 'lastName') {
-                    lastNameValidation.value.error = true;
-                    lastNameValidation.value.errorMessage = 'Illegal character inserted. Plesae use only lowecase and uppercase letter, space, dash and apostrophe';
-                }
+            if (firstNameOk && lastNameOk && birthdateOk && phoneNumberOk) {
+                alert('GOGO POWER RANGERS!!');
+                //TODO: handle event on father
+                emit('goNext', user.value.firstName, user.value.lastName, user.value.birthdate, user.value.phoneNumber);
             }
 
-        return false;
-    }
+            stepValidating.value = disableAll.value = false;
+        }
 
 
         return {
-        //user info
-        user,
-        // validation
-        firstNameValidation,
-        validateName,
-        // general
-        clearError
+            //user info
+            user,
+            // validation
+            firstNameValidation,
+            validateFirstName,
+            lastNameValidation,
+            validateLastName,
+            birthdateValidation,
+            validateBirthdate,
+            phoneNumberValidation,
+            validatePhoneNumber,
+            phoneChangeState,
+            // general
+            disableAll,
+            stepValidating,
+            clearError,
+            goNextStep
+        }
     }
-}
 });
 
 </script>
